df928fb623d6a4a5bba808ddf75b31d6bb700559
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-10-12 15:32:30 -0300

    refactor: improve zipcode validator complexity

--- internal/common/validator.go
+++ internal/common/validator.go
@@ -1,17 +1,9 @@
 package common
 
-import (
-	"regexp"
-)
-
 // ValidateZipCode checks if the given string is a valid Brazilian ZipCode.
 func ValidateZipCode(zipCode string) bool {
+	const _MINIMUN_ZIPCODE_LEN = 8
 	zipCode = StripNonNumericCharacters(zipCode)
 
-	if len(zipCode) != 8 {
-		return false
-	}
-
-	re := regexp.MustCompile(`^\d{5}(-?\d{3})?$`)
-	return re.MatchString(zipCode)
+	return len(zipCode) == _MINIMUN_ZIPCODE_LEN
 }
