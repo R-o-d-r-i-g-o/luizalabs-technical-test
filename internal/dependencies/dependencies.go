655ab2d326a86547b5c27d0e09202e1f37fa6830
R-o-d-r-i-g-o <rodrigomarqribeiro@gmail.com>
2024-10-15 16:04:47 -0300

    feat: add loggers inside project

--- internal/dependencies/dependencies.go
+++ internal/dependencies/dependencies.go
@@ -11,6 +11,7 @@ import (
 	"luizalabs-technical-test/pkg/cache"
 	"luizalabs-technical-test/pkg/crypt"
 	"luizalabs-technical-test/pkg/http"
+	"luizalabs-technical-test/pkg/logger"
 	"luizalabs-technical-test/pkg/postgres"
 	"luizalabs-technical-test/pkg/shutdown"
 	"time"
@@ -28,26 +29,32 @@ func Load() []func(*gin.RouterGroup) {
 	db := loadPostgresDepencies()
 	httpClient := http.NewClient(&netHttp.Client{})
 	cryptHasher := crypt.NewPasswordHasher()
+	logger.Debug("Instanciate internal dependencies...")
 
 	cacheManager := cache.NewManager(cleanupInterval)
 	cacheMiddleware := middleware.NewCacheMiddleware(cacheManager)
 	tokenMiddleware := middleware.NewTokenMiddleware()
+	logger.Debug("Instanciate middleware dependencies...")
 
 	// auth feature
 	authRep := auth.NewRepository(db)
 	authSrv := auth.NewService(authRep, cryptHasher)
 	authHandler := auth.NewHandler(authSrv)
+	logger.Debug("Instanciate auth use-case dependencies...")
 
 	// zipcode feature
 	zipCodeRep := zipcode.NewRepository(httpClient)
 	zipCodeSrv := zipcode.NewService(zipCodeRep)
 	zipCodeHandler := zipcode.NewHandler(zipCodeSrv, cacheMiddleware, tokenMiddleware)
+	logger.Debug("Instanciate zipcode use-case dependencies...")
 
 	// health feature
 	healthHandler := health.NewHandler()
+	logger.Debug("Instanciate health use-case dependencies...")
 
 	// swagger feature
 	swaggerHandler := swagger.NewHandler()
+	logger.Debug("Instanciate swagger use-case dependencies...")
 
 	return []func(*gin.RouterGroup){
 		swaggerHandler.Register,
@@ -61,6 +68,7 @@ func loadPostgresDepencies() *gorm.DB {
 	postgres.SetConnectionString(config.PostgresConfig.ToPostgresDSN())
 	db, err := postgres.GetInstance()
 	if err != nil {
+		logger.Error(err)
 		shutdown.Now()
 	}
 
